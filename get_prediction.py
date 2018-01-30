6d4d55dc9fedfa47acaeb2fb1f9bbf341b6aac20
Ben Whittle <benwhittle31@gmail.com>
2018-01-29 16:24:13 -0800

    removing check for __name__

--- get_prediction.py
+++ get_prediction.py
@@ -1,23 +1,17 @@
 from tensorci import TensorCI
 
+client = TensorCI()
 
-def get_prediction():
-  client = TensorCI()
+unseen_features = {
+  'name': 'Gary',
+  'email': 'gary@gmail.com'
+}
 
-  unseen_features = {
-    'name': 'Gary',
-    'email': 'gary@gmail.com'
-  }
+# Make the prediction
+resp = client.predict(features=unseen_features)
 
-  # Make the prediction
-  resp = client.predict(features=unseen_features)
+# Parse the prediction
+data = resp.json()
 
-  # Parse the prediction
-  data = resp.json()
-
-  # Print the prediction
-  print('Got Prediction:  {}'.format(data.get('prediction')))
-
-
-if __name__ == '__main__':
-  get_prediction()
+# Print the prediction
+print('Got Prediction:  {}'.format(data.get('prediction')))
--- update_dataset.py
+++ update_dataset.py
@@ -1,31 +1,25 @@
 from tensorci import TensorCI
 
+client = TensorCI()
 
-def update_dataset():
-  client = TensorCI()
+# Define new records to add to the dataset
+new_entries = [
+  {
+    'name': 'Tyler',
+    'email': 'tyler@gmail.com'
+  },
+  {
+    'name': 'Sam',
+    'email': 'sam@gmail.com'
+  }
+]
 
-  # Define new records to add to the dataset
-  new_entries = [
-    {
-      'name': 'Tyler',
-      'email': 'tyler@gmail.com'
-    },
-    {
-      'name': 'Sam',
-      'email': 'sam@gmail.com'
-    }
-  ]
+# Update the dataset
+resp = client.update_dataset(records=new_entries)
 
-  # Update the dataset
-  resp = client.update_dataset(records=new_entries)
+# Parse the response
+data = resp.json()
 
-  # Parse the response
-  data = resp.json()
-
-  # Print the response
-  if not data.get('update_error'):
-    print('Dataset successfully updated.')
-
-
-if __name__ == '__main__':
-  update_dataset()
+# Print the response
+if not data.get('update_error'):
+  print('Dataset successfully updated.')
